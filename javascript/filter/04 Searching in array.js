9f2acf02527564df31750f832b67cc41987f846e
Jan Prchal <prchal.jan@outlook.com>
2024-02-21 11:53:22 +0100

    added filter-- searching in array

---
+++ javascript/filter/04 Searching in array.js
@@ -0,0 +1,96 @@
+// Searching in array
+// Following example uses filter() to filter array content based on search criteria
+
+const fruits = ["apple", "banana", "grapes", "mango", "orange"];
+
+// Filter array items based on search criteria (query);
+
+function filterItems(){
+
+}
+
+console.log(filterItems(fruits, "pe"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fruits = ["apple", "banana", "grapes", "mango", "orange"];
+
+
+// function filterItems(array, query){
+//     return array.filter((element)=>
+//         element.toLowerCase().includes(query.toLowerCase()))
+// }
+
+// console.log(filterItems(fruits, "an"));
+
+
+
