70ae3ea05e9614394608d3f75ef9d0a06aab365f
Jan Prchal <prchal.jan@outlook.com>
2024-02-26 07:52:01 +0100

    using arrow functions

--- javascript/Functions/01 Arrow function expressions/01 Using arrow functions.js
+++ javascript/Functions/01 Arrow function expressions/01 Using arrow functions.js
@@ -1,1 +1,42 @@
 // 01 Using arrow functions
+
+// An empty arrow function that returns undefined:
+const empty = () => {};
+
+// Returns "foobar". This is immediately Invoked Function Expression
+console.log((()=>"foobar")())
+
+const simple = (a) => (a > 15 ? 15 : a);
+
+console.log(simple(16));
+console.log(simple(10));
+
+
+const max = (a, b) => (a > b ? a : b);
+
+console.log(max(1, 2));
+console.log(max(224, 99));
+
+
+// Easy array filtering, maping, etc.
+const arr = [5, 6, 13, 0, 1, 18, 23];
+
+const sum = arr.reduce((a, c) => (a + c));
+console.log("sum: ", sum);
+
+const even = arr.filter((c) => c % 2 === 0);
+console.log("even: ", even);
+
+const double = arr.map((c) => c * 2)
+console.log("double: ", double);
+
+// More concise promise chains
+
+// Parameterless arrow functions that are visually easier to parse
+setTimeout(()=>{
+    console.log("I happen sooner");
+    setTimeout(()=>{
+        // deeper code
+        console.log("I happen later")
+    }, 1)
+}, 1)
