45273a172792f3822045a5b8d8a609d16be25305
Jan Prchal <prchal.jan@outlook.com>
2024-02-27 07:30:23 +0100

    reduce function sequential piping

--- javascript/Array.prototype/reduce/04 reduce. Function sequential piping.js
+++ javascript/Array.prototype/reduce/04 reduce. Function sequential piping.js
@@ -5,22 +5,31 @@
 // are called in order, which each one receiving the return value of the previous function.
 
 
-const pipe = null;
+
+
+const pipe = 
+    (...functions) =>
+    (initialValue) =>
+        functions.reduce((acc, fn)=>fn(acc),initialValue)
+
+
+
 // Building blocks to use for composition
-const double = null;
-const triple = null;
-const quadruple = null;
+const double = (x)=>x*2;
+const triple = (x)=>x*3;
+const quadruple = (x)=>x*4;
+const devide = (x)=>x/2;
+const add100 = (x)=>x+100;
+const magnify = (x)=>x*10
 
 // Composed functions for multiplication of specific values.
-const multiply6 = null;
-const multiply9 = null;
-const multiply16 = null;
-const multiply24 = null;
+const orderOfMagnitute = pipe(magnify);
+const twoOrdersOfMagnitute1 = pipe(add100, magnify, devide, magnify);
+const twoOrdersOfMagnitute2 = pipe(add100, magnify, devide,  magnify, );
 
-// console.log(multiply6(6));
-// console.log(multiply9(9));
-// console.log(multiply16(16));
-// console.log(multiply24(10));
+
+console.log(twoOrdersOfMagnitute1(10));
+console.log(twoOrdersOfMagnitute2(10));
 
 
 
