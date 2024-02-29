a7ce405eaf8d7050edeba6f3dbb76a16f026660e
Jan Prchal <prchal.jan@outlook.com>
2024-02-29 09:35:47 +0100

    nesting promises

--- javascript/Promises/00 Using Promises Guide/02 Error Handling.js
+++ javascript/Promises/00 Using Promises Guide/02 Error Handling.js
@@ -1,1 +1,117 @@
 // Error Handling
+
+
+
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
+
+function doSomething(url){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            if(!url){
+                const randomInt = getRandomInt(3);
+                console.log('randomInt: ', randomInt);
+                if(randomInt === 0) {
+                    resolve("https://www.seznam.cz");
+                } else if (randomInt === 1){
+                    reject("Bad Luck!")
+                } else if(randomInt === 2){
+                    reject("Horrible Luck!");
+                }
+            } else {
+                resolve(url + "/didSomething")
+            }
+
+        }, 2000)
+    })
+}
+
+function failureCallback(err){
+    console.log("failureCallback running");
+    console.error(err);
+}
+
+let address;
+doSomething()
+    .then((url)=>{address = url})
+    .then((un)=>{console.log('un: ', un);console.log('address: ', address)})
+    .then((un)=>{console.log("I ran with in: ", un)})
+    .then((un)=>{console.log("I ran with in: ", un)})
+    .catch((()=>(err)=>console.log(err, "custom"))())
+
+
+// You might recall seeing failureCallback three times in the pyramid of doom earlier,
+// compared to only once at the end of the promise chain:
+
+doSomething()
+    .then((url)=>doSomething(url))
+    .then((url)=>doSomething(url))
+    .then((finalResult)=>{console.log(`Got the final result: ${finalResult}`)})
+    .catch(failureCallback)
+
+// If there's an exception, the browser will look down the chain for .catch() handlers
+// or onReject. This is very much modeled after how synchronous code works:
+
+function dooSomething(num){
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            console.log("next step completed")
+            resolve(num + 1)
+        }, 1000)
+    })
+}
+function doSomethingElse(num){
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            console.log("next step completed")
+            resolve(num + 1)
+        }, 1000)
+    })
+}
+function doThirdThing(num){
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            console.log("next step completed")
+            resolve(num + 1)
+        }, 1000)
+    })
+}
+
+function syncDoSomething(num){
+    return num + 1
+}
+function syncDoSomethingElse(num){
+    return num + 1
+}
+function syncDoThirdThing(num){
+    throw new Error("suck it oscar!");
+    return num + 1
+}
+
+try {
+    const result = syncDoSomething(1);
+    const newResult = syncDoSomethingElse(result);
+    const finalResult = syncDoThirdThing(newResult);
+    console.log(`Got the final result: ${finalResult}`);
+} catch (error) {
+    console.log("catch block running");
+    failureCallback(error);
+}
+
+
+// This symetry with asynchronous code culminates in the async / await syntax:
+
+async function foo(num) {
+    try {
+        const result = await dooSomething(num);
+        const newResult = await doSomethingElse(result);
+        const finalResult = await doThirdThing(newResult);
+        console.log(`Got the final result: ${finalResult}`);
+        return finalResult;
+    } catch(error){
+        failureCallback(error);
+    }
+}
+
+foo(1);
---
+++ javascript/Promises/00 Using Promises Guide/03 Nesting (error handling).js
@@ -0,0 +1,79 @@
+// Nesting
+
+// In the examples above involving listOfIngredients, the first one has one promise
+// chain nested in the return value of another then() handler, while the second one
+// uses an entirely flat chain. Simple promise chains are best kept flat without nesting,
+// as nesting can be a result of careless composition.
+
+// Nesting is a control structure to limit the scope of catch statements. Specifically, a 
+// nested catch only catches failures in its scope and below, not errors higher up in
+// the chain outside the nested scope. When used correctly, this gives greater 
+// precision on error recovery:
+
+function doSomethingCritical(num){
+    return new Promise((resolve)=>{
+        resolve(num+1)
+    })
+}
+function doSomethingOptional(num){
+    return new Promise((resolve)=>{
+        resolve(num+1)
+    })
+}
+function doSomethingExtraNice(num){
+    console.log("Did something extra nice");
+    return "SOMETHING EXTRA NICE" + num
+}
+function moreCriticalStuff(){
+    console.log("DID more critical stuff");
+    return "CRITICAL FTUFF"
+}
+
+
+
+
+doSomethingCritical(1)
+    .then((result)=>
+        doSomethingOptional(result)
+            .then((optionalResult)=> doSomethingExtraNice(optionalResult))    
+            .catch((e)=>{})
+    ) // Ignore if optional stuff fails; proceed.
+    .then((niceResult)=>{console.log("niceResult: ", niceResult); return moreCriticalStuff()})
+    .then((result)=>{console.log('result: ', result)})
+    .catch((e)=>console.error(`Critical failure: ${e.message}`))
+
+
+// Note that the optional steps here are nested -- with the nesting caused not by the
+// indentation, but by the placement of the outer ( and ) parentheses around the
+// steps.
+
+// The inner error-silencing catch handler only catches failures from 
+// doSomethingOptional() and doSomethingExtraNice(), after which the code resumes
+// with moreCriticalStuff(). Importantly, if doSomethingCritical() fails, its error is
+// caught by the final ( outer ) catch only, and does not get swallowed by the inner
+// catch handler.
+
+//In async / await, this code looks like:
+
+async function main(){
+    try {
+        const result = await doSomethingCritical();
+        try{
+            const optionalResult = await doSomethingOptional(result);
+            const nestedResult = await doSomethingExtraNice(optionalResult);
+        } catch(e){
+            // Ignore failures in optional steps and proceed.
+        }
+        const result2 = await moreCriticalStuff(nestedResult);
+        console.log(result2);
+    } catch(e) {
+        console.error(`Critical failure: ${e.message}`);
+    }
+}
+
+
+// NOTE: If you don't have sophisticated error handling, you very likely don't
+// need nested then handlers. Instead, use a flat chain and put the error
+// handling logic at the end
+
+
